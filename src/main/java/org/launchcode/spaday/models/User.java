1db4bc0ac5ca204af3d8a56a6f5ad84017e622e8
Heather <>
2023-05-09 21:04:16 -0500

    Studio 5/08/23

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,20 +1,36 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotBlank
+    @Size(min = 5, max = 15)
     private String username;
+    @Email
     private String email;
-    private String password;
+    @NotBlank
+    @Size(min = 6)
+    private String password1;
+    @NotBlank
+    private String password2;
 
-    public User() {
-
-    }
-
-    public User(String username, String email, String password) {
-        this();
+    public User(String username, String email, String password1, String password2) {
         this.username = username;
         this.email = email;
-        this.password = password;
+        this.password1 = password1;
+        this.password2 = password2;
     }
+
+    public User() {
+        this.username = username;
+        this.email = email;
+        this.password1 = password1;
+        this.password2 = password2;
+    }
+
+    //GETTERS AND SETTERS
 
     public String getUsername() {
         return username;
@@ -32,11 +48,19 @@ public class User {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
+    public String getPassword1() {
+        return password1;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setPassword1(String password1) {
+        this.password1 = password1;
+    }
+
+    public String getPassword2() {
+        return password2;
+    }
+
+    public void setPassword2(String password2) {
+        this.password2 = password2;
     }
 }
